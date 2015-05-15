e9d4d2b3a1bb341828a34950e58158901c4669b7
Larry D Gardner <ldgalaxy@comcast.net>
2015-05-14 21:45:57 -0600

    changing to lower case

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php
 
-$string = 'I Could Be Any Case'; 
-
-$string_lower = strtolower($string);
-$string_upper = strtoupper($string);
-
-echo $string_upper;
-
+if(isset($_GET['user_name']) && !empty($_GET['user_name'])){
+   $user_name = $_GET['user_name'];
+  
+   $user_name_lc = strtolower($user_name);
+ 
+   if($user_name_lc=='larry'){
+     echo 'You are the best. ' . $user_name;
+   }
+}
 
 ?>
+
+<form action = "index.php" method = "GET">
+   Name: <input type="text" name="user_name"><br><br>
+   <input type="submit" value="Submit">
+</form>
